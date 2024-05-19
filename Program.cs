efa9a9abe2ba160902543b22b7aef1440b4afd8e
Vinicius Duarte <viniciusrduarte8@gmail.com>
2024-05-19 13:56:11 -0300

    Restricoes de movimento

--- Program.cs
+++ Program.cs
@@ -1,21 +1,32 @@
 ﻿try{
     PartidaDeXadrez partida = new();
     while (!partida.Terminada){
-        Console.Clear();
-        Tela.ImprimirTabuleiro(partida.Tab);
+        try{
+            Console.Clear();
+            Tela.ImprimirTabuleiro(partida.Tab);
+            System.Console.WriteLine();
+            System.Console.WriteLine("Turno: " + partida.Turno);
+            System.Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
 
-        System.Console.Write("Origem: ");
-        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+            System.Console.Write("Origem: ");
+            Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+            partida.ValidarPosicaoDeOrigem(origem);
 
-        bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
+            bool[,] posicoesPossiveis = partida.Tab.Peca(origem).MovimentosPossiveis();
 
-        Console.Clear();
-        Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
+            Console.Clear();
+            Tela.ImprimirTabuleiro(partida.Tab, posicoesPossiveis);
 
-        System.Console.WriteLine();
-        System.Console.Write("Destino: ");
-        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
-        partida.ExecutaMovimento(origem, destino);
+            System.Console.WriteLine();
+            System.Console.Write("Destino: ");
+            Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+            partida.ValidarPosicaoDeDestino(origem, destino);
+            partida.RealizaJogada(origem, destino);
+        }
+        catch(TabuleiroException e){
+            System.Console.WriteLine(e.Message);
+            Console.ReadLine();
+        }
     }
 }
 catch (TabuleiroException e){
