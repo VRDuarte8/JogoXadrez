2f046b52dd74b770707d8b808efc950855ddbb88
Vinicius Duarte <viniciusrduarte8@gmail.com>
2024-05-19 21:26:22 -0300

    Acrescentando todas as pecas

--- obj/Debug/net8.0/JogoXadrez.AssemblyInfo.cs
+++ obj/Debug/net8.0/JogoXadrez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoXadrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d17e286d0ff597e22e0ecfbff4ca3111fcd1642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930605e01740576a9fca07be520dada8dd6a0651")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoXadrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoXadrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- xadrez/Dama.cs
+++ xadrez/Dama.cs
@@ -1,5 +1,5 @@
-class Bispo : Peca {
-    public Bispo(Tabuleiro tab, Cor cor) : base(tab, cor){
+class Dama : Peca {
+    public Dama(Tabuleiro tab, Cor cor) : base(tab, cor){
     }
 
     private bool PodeMover(Posicao pos){
@@ -11,6 +11,43 @@ class Bispo : Peca {
     {
         bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
         Posicao pos = new Posicao(0,0);
+
+        pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+        while (Tab.PosicaoValida(pos) && PodeMover(pos))
+        {
+            mat[pos.Linha, pos.Coluna] = true;
+            if(Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor){
+                break;
+            }
+            pos.Linha = pos.Linha - 1;
+        }
+        pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+        while (Tab.PosicaoValida(pos) && PodeMover(pos))
+        {
+            mat[pos.Linha, pos.Coluna] = true;
+            if(Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor){
+                break;
+            }
+            pos.Linha = pos.Linha + 1;
+        }
+        pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+        while (Tab.PosicaoValida(pos) && PodeMover(pos))
+        {
+            mat[pos.Linha, pos.Coluna] = true;
+            if(Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor){
+                break;
+            }
+            pos.Coluna = pos.Coluna + 1;
+        }
+        pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+        while (Tab.PosicaoValida(pos) && PodeMover(pos))
+        {
+            mat[pos.Linha, pos.Coluna] = true;
+            if(Tab.Peca(pos) != null && Tab.Peca(pos).Cor != Cor){
+                break;
+            }
+            pos.Coluna = pos.Coluna - 1;
+        }
 
         pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
         while (Tab.PosicaoValida(pos) && PodeMover(pos))
@@ -55,6 +92,6 @@ class Bispo : Peca {
 
     public override string ToString()
     {
-        return "B";
+        return "D";
     }
 }
