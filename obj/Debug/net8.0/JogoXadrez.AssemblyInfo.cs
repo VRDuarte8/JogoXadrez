d0df459d6c3f2e668d1c29b810a3f3a39671582e
Vinicius Duarte <viniciusrduarte8@gmail.com>
2024-05-19 23:57:16 -0300

    Jogada especial: promocao

--- obj/Debug/net8.0/JogoXadrez.AssemblyInfo.cs
+++ obj/Debug/net8.0/JogoXadrez.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JogoXadrez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb572f8af24b823f6ea83145a8aa0dc6c22fe38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4267e18123944271ec1fcd9a8bd200302eac3ffe")]
 [assembly: System.Reflection.AssemblyProductAttribute("JogoXadrez")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JogoXadrez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
